035da8123bdcc7a644f1023e127157f02111fbb8
jjis936 <evanjoyner2012@gmail.com>
2026-07-11 17:26:54 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,6 +3,16 @@ require("dotenv").config();
 const { REST, Routes } = require("discord.js");
 
 
+// YOUR IDS
+
+const CLIENT_ID = "1525205213827436635";
+
+const GUILD_ID = "1500601982740856875";
+
+
+
+// LOAD COMMANDS
+
 const commands = [
 
     require("./commands/panel").data.toJSON(),
@@ -19,31 +29,37 @@ const commands = [
 
 
 
+// DISCORD REST
+
 const rest = new REST({
 
-    version:"10"
+    version: "10"
 
 }).setToken(process.env.TOKEN);
 
 
+
+// DEPLOY
 
 (async()=>{
 
     try{
 
 
-        console.log("⏳ Deploying Sinner Services commands...");
+        console.log(
+            "⏳ Deploying Sinner Services commands..."
+        );
 
 
         console.log(
             "Client ID:",
-            process.env.CLIENT_ID
+            CLIENT_ID
         );
 
 
         console.log(
             "Guild ID:",
-            process.env.GUILD_ID
+            GUILD_ID
         );
 
 
@@ -52,15 +68,15 @@ const rest = new REST({
 
             Routes.applicationGuildCommands(
 
-                process.env.CLIENT_ID,
+                CLIENT_ID,
 
-                process.env.GUILD_ID
+                GUILD_ID
 
             ),
 
             {
 
-                body:commands
+                body: commands
 
             }
 
