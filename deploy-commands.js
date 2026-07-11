10b66571b8b75f3248a22e6d53ba8c5d211aebd1
jjis936 <evanjoyner2012@gmail.com>
2026-07-11 11:25:47 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,47 +7,69 @@ const {
 } = require("discord.js");
 
 
-console.log("CLIENT_ID:", process.env.CLIENT_ID);
-console.log("GUILD_ID:", process.env.GUILD_ID);
-
-
+// ================================
+// COMMANDS
+// ================================
 
 const commands = [
 
 
-new SlashCommandBuilder()
-.setName("panel")
-.setDescription("Create the Sinner Services vouch panel")
-.toJSON(),
+    new SlashCommandBuilder()
+
+    .setName("panel")
+
+    .setDescription(
+        "Create the Sinner Services vouch panel"
+    )
+
+    .toJSON(),
 
 
 
-new SlashCommandBuilder()
-.setName("vouchstats")
-.setDescription("View Sinner Services statistics")
-.toJSON(),
+    new SlashCommandBuilder()
+
+    .setName("vouchstats")
+
+    .setDescription(
+        "View Sinner Services statistics"
+    )
+
+    .toJSON(),
 
 
 
-new SlashCommandBuilder()
-.setName("leaderboard")
-.setDescription("View top customers")
-.toJSON()
+    new SlashCommandBuilder()
+
+    .setName("leaderboard")
+
+    .setDescription(
+        "View top Sinner Services customers"
+    )
+
+    .toJSON()
 
 
 ];
 
 
 
+// ================================
+// DISCORD API
+// ================================
+
 const rest = new REST({
 
-version:"10"
+    version: "10"
 
 }).setToken(
-process.env.TOKEN
+    process.env.TOKEN
 );
 
 
+
+// ================================
+// DEPLOY
+// ================================
 
 (async()=>{
 
@@ -56,42 +78,66 @@ try{
 
 
 console.log(
-"⏳ Deploying server commands..."
+"⏳ Deploying Sinner Services commands..."
+);
+
+
+
+console.log(
+"Client ID:",
+process.env.CLIENT_ID
+);
+
+
+console.log(
+"Guild ID:",
+process.env.GUILD_ID
 );
 
 
 
 await rest.put(
 
+
 Routes.applicationGuildCommands(
 
-process.env.CLIENT_ID,
+    process.env.CLIENT_ID,
 
-process.env.GUILD_ID
+    process.env.GUILD_ID
 
 ),
 
+
 {
 
-body:commands
+    body: commands
 
 }
+
 
 );
 
 
 
 console.log(
-"✅ Server commands deployed!"
+"✅ Sinner Services commands deployed!"
 );
 
 
 
 }
 
+
 catch(error){
 
+
+console.log(
+"❌ Deploy Error:"
+);
+
+
 console.log(error);
+
 
 }
 
