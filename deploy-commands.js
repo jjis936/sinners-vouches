3825feb8e6d309391bea979b4db345a3ca41d04b
jjis936 <evanjoyner2012@gmail.com>
2026-07-11 08:30:21 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,90 @@
+require("dotenv").config();
 
+const {
+    REST,
+    Routes
+} = require("discord.js");
+
+const {
+    SlashCommandBuilder
+} = require("discord.js");
+
+
+// Commands list
+
+const commands = [
+
+    new SlashCommandBuilder()
+
+    .setName("panel")
+
+    .setDescription(
+        "Create the Sinner Services vouch panel"
+    )
+
+    .toJSON()
+
+
+];
+
+
+
+// Discord API connection
+
+const rest = new REST({
+
+    version: "10"
+
+}).setToken(
+    process.env.TOKEN
+);
+
+
+
+// Deploy command
+
+(async () => {
+
+
+    try {
+
+
+        console.log(
+            "⏳ Deploying commands..."
+        );
+
+
+        await rest.put(
+
+            Routes.applicationCommands(
+                process.env.CLIENT_ID
+            ),
+
+            {
+
+                body: commands
+
+            }
+
+        );
+
+
+
+        console.log(
+            "✅ Commands deployed!"
+        );
+
+
+    }
+
+
+    catch(error){
+
+
+        console.log(error);
+
+
+    }
+
+
+})();
