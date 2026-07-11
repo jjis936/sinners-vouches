99f47f7a66743dd08b9d49bdf2c594b53cfa2842
jjis936 <evanjoyner2012@gmail.com>
2026-07-11 16:42:41 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,33 +1,129 @@
 require("dotenv").config();
 
-const { REST, Routes } = require("discord.js");
+const {
+    REST,
+    Routes
+} = require("discord.js");
 
-const commands = [
-    require("./commands/panel").data.toJSON(),
-    require("./commands/vouchstats").data.toJSON(),
-    require("./commands/post").data.toJSON(),
-    require("./commands/embed").data.toJSON()
-];
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const TOKEN = process.env.TOKEN;
+// ================================
+// YOUR SERVER INFO
+// ================================
+
+const CLIENT_ID = "1525205213827436635";
+
 const GUILD_ID = "1500601982740856875";
 
-const rest = new REST({ version: "10" }).setToken(TOKEN);
 
-(async () => {
+
+// ================================
+// COMMANDS
+// ================================
+
+const commands = [
+
+    require("./commands/panel")
+    .data.toJSON(),
+
+    require("./commands/vouchstats")
+    .data.toJSON(),
+
+    require("./commands/post")
+    .data.toJSON(),
+
+    require("./commands/embed")
+    .data.toJSON(),
+
+    require("./commands/ban")
+    .data.toJSON()
+
+];
+
+
+
+// ================================
+// DISCORD REST
+// ================================
+
+const rest = new REST({
+
+    version: "10"
+
+}).setToken(
+
+    process.env.TOKEN
+
+);
+
+
+
+// ================================
+// DEPLOY COMMANDS
+// ================================
+
+(async()=>{
+
     try {
-        console.log("⏳ Deploying commands...");
-        console.log("Client ID:", CLIENT_ID);
-        console.log("Guild ID:", GUILD_ID);
 
-        await rest.put(
-            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-            { body: commands }
+
+        console.log(
+            "⏳ Deploying Sinner Services commands..."
         );
 
-        console.log("✅ Commands deployed successfully!");
-    } catch (error) {
-        console.error(error);
+
+        console.log(
+            "Client ID:",
+            CLIENT_ID
+        );
+
+
+        console.log(
+            "Guild ID:",
+            GUILD_ID
+        );
+
+
+
+        await rest.put(
+
+            Routes.applicationGuildCommands(
+
+                CLIENT_ID,
+
+                GUILD_ID
+
+            ),
+
+            {
+
+                body: commands
+
+            }
+
+        );
+
+
+
+        console.log(
+            "✅ Commands deployed successfully!"
+        );
+
+
+
+    } catch(error){
+
+
+        console.error(
+            "❌ Deploy Error:"
+        );
+
+
+        console.error(
+            error
+        );
+
+
     }
+
+
 })();
