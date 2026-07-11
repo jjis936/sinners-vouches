3317995a70a64556044d1d61bb2b666291d1058d
jjis936 <evanjoyner2012@gmail.com>
2026-07-11 12:09:36 -0400

    Update post.js

--- commands/post.js
+++ commands/post.js
@@ -1,7 +1,10 @@
 const {
     SlashCommandBuilder,
     EmbedBuilder,
-    PermissionFlagsBits
+    PermissionFlagsBits,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle
 } = require("discord.js");
 
 const config = require("../config");
@@ -15,132 +18,4 @@ data: new SlashCommandBuilder()
 .setName("post")
 
 .setDescription(
-    "Create a Sinner Services announcement embed"
-)
-
-.addStringOption(option =>
-
-    option
-
-    .setName("title")
-
-    .setDescription("Announcement title")
-
-    .setRequired(true)
-
-)
-
-
-.addStringOption(option =>
-
-    option
-
-    .setName("message")
-
-    .setDescription("Announcement message")
-
-    .setRequired(true)
-
-),
-
-
-
-async execute(interaction){
-
-
-
-if(
-!interaction.member.permissions.has(
-PermissionFlagsBits.ManageMessages
-)
-
-){
-
-return interaction.reply({
-
-content:
-"❌ You do not have permission to use this command.",
-
-ephemeral:true
-
-});
-
-}
-
-
-
-const title =
-interaction.options.getString(
-"title"
-);
-
-
-
-const message =
-interaction.options.getString(
-"message"
-);
-
-
-
-const embed =
-new EmbedBuilder()
-
-.setColor(
-config.COLORS.RED
-)
-
-.setTitle(
-`💎 ${title}`
-)
-
-.setDescription(
-
-`
-${message}
-
-
-━━━━━━━━━━━━━━
-
-💎 **Sinner Services**
-
-`
-
-)
-
-.setTimestamp()
-
-.setFooter({
-
-text:
-"Sinner Services"
-
-});
-
-
-
-await interaction.reply({
-
-content:
-"✅ Announcement posted!",
-
-ephemeral:true
-
-});
-
-
-
-await interaction.channel.send({
-
-embeds:[
-embed
-]
-
-});
-
-
-
-}
-
-
-};
+"Create a
