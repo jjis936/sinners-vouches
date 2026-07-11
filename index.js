cf2d5900fd04d7f8c9579424b4e3e47919281f5b
jjis936 <evanjoyner2012@gmail.com>
2026-07-11 08:28:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,171 @@
 require("dotenv").config();
 
 const {
-Client,
-GatewayIntentBits,
-Collection
+    Client,
+    GatewayIntentBits,
+    Collection
 } = require("discord.js");
-
 
 const config = require("./config");
 
 
+// ================================
+// CREATE CLIENT
+// ================================
+
 const client = new Client({
 
-    intents:[
+    intents: [
 
         GatewayIntentBits.Guilds,
-
-        GatewayIntentBits.GuildMessages,
-
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.GuildMessages
 
     ]
 
 });
 
 
+// ================================
+// COMMAND SYSTEM
+// ================================
 
 client.commands = new Collection();
 
 
+// Load Commands
 
-client.once("ready",()=>{
+const panel = require("./commands/panel");
 
 
-console.log(
-`💎 ${client.user.tag} is online`
+client.commands.set(
+    panel.data.name,
+    panel
 );
 
 
-client.user.setActivity(
-"Sinner Services V2 | Vouches"
-);
+
+// ================================
+// BOT ONLINE
+// ================================
+
+client.once("ready", () => {
+
+
+    console.log(
+        `💎 ${client.user.tag} is online`
+    );
+
+
+    client.user.setPresence({
+
+        activities: [
+
+            {
+
+                name:
+                "Sinner Services V2 | Vouches"
+
+            }
+
+        ],
+
+        status:
+        "online"
+
+    });
 
 
 });
 
 
 
+// ================================
+// COMMAND HANDLER
+// ================================
+
+client.on(
+"interactionCreate",
+async interaction => {
 
 
-client.login(config.TOKEN);
+    // Slash commands
+
+    if(
+        interaction.isChatInputCommand()
+    ){
+
+        const command =
+        client.commands.get(
+            interaction.commandName
+        );
+
+
+        if(!command)
+            return;
+
+
+
+        try {
+
+
+            await command.execute(
+                interaction
+            );
+
+
+        }
+
+
+        catch(error){
+
+
+            console.log(error);
+
+
+
+            await interaction.reply({
+
+                content:
+                "❌ Command error.",
+
+                ephemeral:true
+
+            });
+
+
+        }
+
+
+    }
+
+
+
+    // Buttons will go here later
+
+
+    if(
+        interaction.isButton()
+    ){
+
+
+        console.log(
+            "Button clicked:",
+            interaction.customId
+        );
+
+
+    }
+
+
+
+});
+
+
+
+// ================================
+// LOGIN
+// ================================
+
+client.login(
+    config.TOKEN
+);
