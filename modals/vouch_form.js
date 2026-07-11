301ab49b16eb15872bb528f8a9d5dc919dece946
jjis936 <evanjoyner2012@gmail.com>
2026-07-11 09:42:02 -0400

    Update vouch_form.js

--- modals/vouch_form.js
+++ modals/vouch_form.js
@@ -13,30 +13,51 @@ async execute(interaction){
 
 
 const rating =
-interaction.fields.getTextInputValue(
-"rating"
-);
+interaction.fields.getTextInputValue("rating");
 
 
 const feedback =
-interaction.fields.getTextInputValue(
-"feedback"
-);
+interaction.fields.getTextInputValue("feedback");
 
 
 const service =
-interaction.fields.getTextInputValue(
-"service"
-);
+interaction.fields.getTextInputValue("service");
+
+
+
+const number =
+Number(rating);
+
+
+
+if(
+number < 1 ||
+number > 5 ||
+isNaN(number)
+){
+
+return interaction.reply({
+
+content:
+"❌ Rating must be between 1 and 5.",
+
+ephemeral:true
+
+});
+
+}
+
 
 
 
 let data = {};
 
 
+
 if(fs.existsSync(path)){
 
-data = JSON.parse(
+data =
+JSON.parse(
 fs.readFileSync(path)
 );
 
@@ -44,15 +65,34 @@ fs.readFileSync(path)
 
 
 
+if(data[interaction.user.id]){
+
+
+return interaction.reply({
+
+content:
+"⚠️ You already have a pending vouch.",
+
+ephemeral:true
+
+});
+
+
+}
+
+
+
 data[interaction.user.id] = {
 
-rating: rating,
 
-feedback: feedback,
+rating:number,
 
-service: service,
+feedback:feedback,
 
-time: Date.now()
+service:service,
+
+created:Date.now()
+
 
 };
 
@@ -75,14 +115,15 @@ null,
 await interaction.reply({
 
 content:
-"✅ Vouch information saved!\n\n📸 Now send your proof screenshot or video in this channel.",
+
+"✅ Review saved!\n\n📸 Please upload your proof screenshot/video.",
 
 ephemeral:true
 
 });
 
 
-
 }
+
 
 };
