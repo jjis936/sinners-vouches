59b1be5157439a1d7b2da1cbc7e7af53615385cb
jjis936 <evanjoyner2012@gmail.com>
2026-07-11 08:22:16 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,29 @@
+module.exports = {
 
+    TOKEN: process.env.TOKEN,
+
+    CLIENT_ID: process.env.CLIENT_ID,
+
+    GUILD_ID: process.env.GUILD_ID,
+
+
+    COLORS: {
+
+        RED: "#B30000",
+
+        DARK: "#111111",
+
+        WHITE: "#FFFFFF"
+
+    },
+
+
+    BRAND: {
+
+        NAME: "Sinner Services",
+
+        EMOJI: "💎"
+
+    }
+
+};
