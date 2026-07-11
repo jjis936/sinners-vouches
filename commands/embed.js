1310e143a9195d1c0dceee5bebeac549e0dce74b
jjis936 <evanjoyner2012@gmail.com>
2026-07-11 16:27:14 -0400

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -1,16 +1,8 @@
 const {
     SlashCommandBuilder,
     EmbedBuilder,
-    PermissionFlagsBits,
-    ChannelType
+    PermissionFlagsBits
 } = require("discord.js");
-
-const config = require("../config");
-
-const {
-    createButtons
-} = require("../buttons/embed_buttons");
-
 
 module.exports = {
 
@@ -19,16 +11,6 @@ module.exports = {
         .setName("embed")
         .setDescription("Create a premium announcement embed")
 
-
-        .addChannelOption(option =>
-            option
-                .setName("channel")
-                .setDescription("Channel to send the embed")
-                .addChannelTypes(ChannelType.GuildText)
-                .setRequired(true)
-        )
-
-
         .addStringOption(option =>
             option
                 .setName("title")
@@ -36,97 +18,37 @@ module.exports = {
                 .setRequired(true)
         )
 
-
         .addStringOption(option =>
             option
                 .setName("description")
-                .setDescription("Embed description")
+                .setDescription("Embed message")
                 .setRequired(true)
         )
-
 
         .addStringOption(option =>
             option
                 .setName("color")
-                .setDescription("Embed color")
+                .setDescription("Embed color hex (#B30000)")
                 .setRequired(false)
         )
 
+        .addAttachmentOption(option =>
+            option
+                .setName("picture")
+                .setDescription("Upload an image")
+                .setRequired(false)
+        )
 
         .addStringOption(option =>
             option
                 .setName("footer")
                 .setDescription("Footer text")
                 .setRequired(false)
-        )
-
-
-        .addStringOption(option =>
-            option
-                .setName("image")
-                .setDescription("Large image URL")
-                .setRequired(false)
-        )
-
-
-        .addStringOption(option =>
-            option
-                .setName("thumbnail")
-                .setDescription("Thumbnail URL")
-                .setRequired(false)
-        )
-
-
-        .addStringOption(option =>
-            option
-                .setName("mention")
-                .setDescription("Mention everyone or here")
-                .addChoices(
-                    {
-                        name:"Everyone",
-                        value:"everyone"
-                    },
-                    {
-                        name:"Here",
-                        value:"here"
-                    }
-                )
-                .setRequired(false)
-        )
-
-
-        .addStringOption(option =>
-            option
-                .setName("button1")
-                .setDescription(
-                    "Example: Open Ticket|button|ticket"
-                )
-                .setRequired(false)
-        )
-
-
-        .addStringOption(option =>
-            option
-                .setName("button2")
-                .setDescription(
-                    "Example: Leave Vouch|button|leave_vouch"
-                )
-                .setRequired(false)
-        )
-
-
-        .addStringOption(option =>
-            option
-                .setName("button3")
-                .setDescription(
-                    "Example: Website|link|https://example.com"
-                )
-                .setRequired(false)
         ),
 
 
 
-    async execute(interaction){
+    async execute(interaction) {
 
 
         if(
@@ -137,9 +59,7 @@ module.exports = {
 
             return interaction.reply({
 
-                content:
-                "❌ You need Manage Messages permission.",
-
+                content:"❌ You don't have permission.",
                 ephemeral:true
 
             });
@@ -148,92 +68,35 @@ module.exports = {
 
 
 
-        const channel =
-        interaction.options.getChannel(
-            "channel"
-        );
-
-
         const title =
-        interaction.options.getString(
-            "title"
-        );
+        interaction.options.getString("title");
 
 
         const description =
-        interaction.options.getString(
-            "description"
-        );
+        interaction.options.getString("description");
 
 
         const color =
-        interaction.options.getString(
-            "color"
-        )
-        ||
-        config.COLORS.RED;
-
+        interaction.options.getString("color") || "#B30000";
 
 
         const footer =
-        interaction.options.getString(
-            "footer"
-        );
+        interaction.options.getString("footer");
 
 
-        const image =
-        interaction.options.getString(
-            "image"
-        );
-
-
-        const thumbnail =
-        interaction.options.getString(
-            "thumbnail"
-        );
-
-
-        const mention =
-        interaction.options.getString(
-            "mention"
-        );
-
-
-
-        const buttons = [
-
-            interaction.options.getString(
-                "button1"
-            ),
-
-            interaction.options.getString(
-                "button2"
-            ),
-
-            interaction.options.getString(
-                "button3"
-            )
-
-        ].filter(Boolean);
+        const picture =
+        interaction.options.getAttachment("picture");
 
 
 
         const embed =
         new EmbedBuilder()
 
-
         .setColor(color)
 
+        .setTitle(`💎 ${title}`)
 
-        .setTitle(
-            `💎 ${title}`
-        )
-
-
-        .setDescription(
-            description
-        )
-
+        .setDescription(description)
 
         .setTimestamp();
 
@@ -250,61 +113,20 @@ module.exports = {
         }
 
 
-        if(image){
 
-            embed.setImage(image);
+        if(picture){
 
-        }
-
-
-        if(thumbnail){
-
-            embed.setThumbnail(thumbnail);
-
-        }
-
-
-
-        let content = null;
-
-
-
-        if(mention === "everyone"){
-
-            content = "@everyone";
-
-        }
-
-
-        if(mention === "here"){
-
-            content = "@here";
-
-        }
-
-
-
-        let components = [];
-
-
-
-        if(buttons.length > 0){
-
-            components.push(
-                createButtons(buttons)
+            embed.setImage(
+                picture.url
             );
 
         }
 
 
 
-        await channel.send({
+        await interaction.channel.send({
 
-            content,
-
-            embeds:[embed],
-
-            components
+            embeds:[embed]
 
         });
 
@@ -312,9 +134,7 @@ module.exports = {
 
         await interaction.reply({
 
-            content:
-            "✅ Premium embed with buttons posted!",
-
+            content:"✅ Premium embed posted!",
             ephemeral:true
 
         });
